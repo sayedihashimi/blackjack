fc6f95bd7df6644ab6521d82cce363f77af36263
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2023-02-18 15:29:31 -0500

    adding header to files

--- src/SayedHa.Roulette.Cli/DefaultGameSettingsFile.cs
+++ src/SayedHa.Roulette.Cli/DefaultGameSettingsFile.cs
@@ -1,4 +1,18 @@
-﻿using SayedHa.Blackjack.Shared;
+﻿// This file is part of SayedHa.Blackjack.
+//
+// SayedHa.Blackjack is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// SayedHa.Blackjack is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with SayedHa.Blackjack.  If not, see <https://www.gnu.org/licenses/>.
+using SayedHa.Blackjack.Shared;
 using SayedHa.Blackjack.Shared.Roulette;
 using System;
 using System.Collections.Generic;
